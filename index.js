3ad116783d6e781c9a084593b16bbe5ed06f3909
Karan Kewat <84654445+karankewat02@users.noreply.github.com>
2022-05-04 02:22:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const rwClient = require("./twitterClient.js");
 const fetch = require('node-fetch');
-
 const express = require('express');
 const res = require("express/lib/response");
-
 const app = express();
-
 app.listen(process.env.PORT || 4000 , ()=>{
     console.log("app listenting on port 4000")
 })
@@ -41,37 +38,6 @@ app.get("/",(req,res)=>{
 
 })
 
-
-// https://picsum.photos/1600/900
-
-// const tweet = async () => {
-
-//     const rand = Math.floor(Math.random()*(200-0+1)+0);   
-//     const response = await fetch("https://programming-quotes-api.herokuapp.com/quotes")
-//     const quotes = await response.json();
-
-//     if(response){
-
-//         try {
-//             await rwClient.v1.uploadMedia('./ShubParshuramJayanti.png');
-//             await rwClient.v1.tweet(`Good Morning :)\n\n${quotes[rand].en}\n\nBy - ${quotes[rand].author}`)  
-//             console.log("tweeted")     
-//         } catch (error) {
-//             console.error(error)
-//         }
-    
-//     }else{
-//         try {
-//             await rwClient.v1.tweet("Tweeted via bot !!")        
-//         } catch (error) {
-//             console.error(error)
-//         }
-    
-//     }
-
-// }
-
-// tweet();
 
 // const job = new cronJob("* * * * *", ()=>{
 //     tweet();
